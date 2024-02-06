c5d8455959f8ceab8be152afb301ba08329acb06
E3Ha <emma.harrison@talawanda.org>
2024-02-06 13:58:14 -0500

    2/6/2024 1:58

--- src/Practice5.java
+++ src/Practice5.java
@@ -2,7 +2,7 @@ public class Practice5 {
 
     public static void main(String[] args){
 
-        System.out.print("*" + "E" + "*" + "M" + "*" + "M" + "*" + "A" +"*");
+        System.out.print("*E*M*M*A* *H*A*R*R*I*S*O*N*");
 
 
     }
